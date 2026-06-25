9167cd3920321651a888f51f099e8db87f1d38f3
padmavathyprasannakumar <padmavathyprasannakumar@gmail.com>
2026-06-25 16:22:32 +0800

    Update settings.py

--- travel_booking_site/settings.py
+++ travel_booking_site/settings.py
@@ -194,7 +194,7 @@ STORAGES = {
 # --------------------------------------------------
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.environ.get("MEDIA_ROOT", BASE_DIR / "media")
 
 
 # --------------------------------------------------
